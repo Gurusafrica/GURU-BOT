54681ed25b3fa05fb891d3203f460f54de94c3b3
Gurusafrica <153798078+Gurusafrica@users.noreply.github.com>
2024-01-19 04:13:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['234 9012162899', 'GURUs', true],
+  ['234 9012162899', 'gurus', true], 
   [''] 
 ] //Number of owners
 
